9b2816c9f3b9555b39e6b0a1ca63aa7252f3b37f
Hiroki Chiyoda <contact@arafipro.com>
2024-01-04 13:21:44 +0900

    記事取得のAPIを作成#17

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -4,6 +4,22 @@ import { NextResponse } from "next/server";
 
 export const runtime = "edge";
 
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id;
+  try {
+    await connect();
+    const post = await prisma.post.findFirst({ where: { id } });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await disconnect();
+  }
+}
+
 export async function DELETE(
   request: Request,
   { params }: { params: { id: string } }
