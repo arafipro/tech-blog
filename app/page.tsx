364be95fff6844616fd72a9d1146cf4966deebad
Hiroki Chiyoda <contact@arafipro.com>
2024-01-04 17:21:11 +0900

    Postテーブルから全データを取得してトップページに表示#27

--- app/page.tsx
+++ app/page.tsx
@@ -1,98 +1,32 @@
+"use client";
+
 import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { getAllPosts } from "@/lib/blogApi";
+import { dateFormat } from "@/utils/dateFormat";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = await getAllPosts();
+      setPosts(data.posts);
+    }
+    fetchPosts();
+  }, []);
   return (
     <section className="flex flex-wrap justify-center">
-      <Card className="m-4 w-96">
-        <CardHeader>
-          <CardTitle className="text-center mb-10">Title</CardTitle>
-        </CardHeader>
-        <CardFooter className="flex gap-x-4 justify-center">
-          <p>Created At</p>
-          <p>Updated At</p>
-        </CardFooter>
-      </Card>
-      <Card className="m-4 w-96">
-        <CardHeader>
-          <CardTitle className="text-center mb-10">Title</CardTitle>
-        </CardHeader>
-        <CardFooter className="flex gap-x-4 justify-center">
-          <p>Created At</p>
-          <p>Updated At</p>
-        </CardFooter>
-      </Card>
-      <Card className="m-4 w-96">
-        <CardHeader>
-          <CardTitle className="text-center mb-10">Title</CardTitle>
-        </CardHeader>
-        <CardFooter className="flex gap-x-4 justify-center">
-          <p>Created At</p>
-          <p>Updated At</p>
-        </CardFooter>
-      </Card>
-      <Card className="m-4 w-96">
-        <CardHeader>
-          <CardTitle className="text-center mb-10">Title</CardTitle>
-        </CardHeader>
-        <CardFooter className="flex gap-x-4 justify-center">
-          <p>Created At</p>
-          <p>Updated At</p>
-        </CardFooter>
-      </Card>
-      <Card className="m-4 w-96">
-        <CardHeader>
-          <CardTitle className="text-center mb-10">Title</CardTitle>
-        </CardHeader>
-        <CardFooter className="flex gap-x-4 justify-center">
-          <p>Created At</p>
-          <p>Updated At</p>
-        </CardFooter>
-      </Card>
-      <Card className="m-4 w-96">
-        <CardHeader>
-          <CardTitle className="text-center mb-10">Title</CardTitle>
-        </CardHeader>
-        <CardFooter className="flex gap-x-4 justify-center">
-          <p>Created At</p>
-          <p>Updated At</p>
-        </CardFooter>
-      </Card>
-      <Card className="m-4 w-96">
-        <CardHeader>
-          <CardTitle className="text-center mb-10">Title</CardTitle>
-        </CardHeader>
-        <CardFooter className="flex gap-x-4 justify-center">
-          <p>Created At</p>
-          <p>Updated At</p>
-        </CardFooter>
-      </Card>
-      <Card className="m-4 w-96">
-        <CardHeader>
-          <CardTitle className="text-center mb-10">Title</CardTitle>
-        </CardHeader>
-        <CardFooter className="flex gap-x-4 justify-center">
-          <p>Created At</p>
-          <p>Updated At</p>
-        </CardFooter>
-      </Card>
-      <Card className="m-4 w-96">
-        <CardHeader>
-          <CardTitle className="text-center mb-10">Title</CardTitle>
-        </CardHeader>
-        <CardFooter className="flex gap-x-4 justify-center">
-          <p>Created At</p>
-          <p>Updated At</p>
-        </CardFooter>
-      </Card>
-      <Card className="m-4 w-96">
-        <CardHeader>
-          <CardTitle className="text-center mb-10">Title</CardTitle>
-        </CardHeader>
-        <CardFooter className="flex gap-x-4 justify-center">
-          <p>Created At</p>
-          <p>Updated At</p>
-        </CardFooter>
-      </Card>
+      {posts.map((post: Post) => (
+        <Card className="m-4 w-96" key={post.id}>
+          <CardHeader>
+            <CardTitle className="text-center mb-10">{post.title}</CardTitle>
+          </CardHeader>
+          <CardFooter className="flex gap-x-4 justify-center">
+            <p>{dateFormat(post.createAt)}</p>
+            <p>{dateFormat(post.updateAt)}</p>
+          </CardFooter>
+        </Card>
+      ))}
     </section>
   );
 }
