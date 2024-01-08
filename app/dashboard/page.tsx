05377931797a48c95f0bede5a750de77cddfdd45
Hiroki Chiyoda <contact@arafipro.com>
2024-01-08 15:35:25 +0900

    ルーティングを変更#33

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,15 +4,12 @@ import { useSession } from "next-auth/react";
 
 export default function Page() {
   const { data: session } = useSession();
-	
-	if (session && session.user) {
-    return (
-			<div className="text-center">
-      <h1 className="text-5xl mb-10">Dashboard</h1>
-      <p className="text-3xl">Welcome to {session!.user!.name}</p>
-    </div>
 
-    );
-  }
+  return (
+    <p className="text-center text-3xl">
+      {session && session.user
+        ? `Welcome to ${session!.user!.name}`
+        : "Please Login!!"}
+    </p>
+  );
 }
-
--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,7 @@ import authConfig from "@/auth.config";
 import NextAuth from "next-auth";
 const { auth } = NextAuth(authConfig);
 
-export default auth((req) => {
-  console.log(`ROUTE:${req.nextUrl.hostname}`);
-});
+export default auth((req) => {});
 
 // Optionally, don't invoke Middleware on some paths
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
